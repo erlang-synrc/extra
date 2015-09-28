e9baf89d0f291f1be311cc3720b2f78e95ec4c94
Namdak Tonpa <maxim@synrc.com>
2015-09-29 00:38:13 +0300

    fix extra

--- include/extra.hrl
+++ include/extra.hrl
@@ -23,10 +23,8 @@
 -record(slider, {?ELEMENT_BASE(element_slider), min, max, step, orientation, value, selection, tooltip, handle, formater}).
 
 % Synrc Elements
--record(rtable, {?ELEMENT_BASE(element_rtable), rows=[], postback}).
--record(upload_state, {dir, cid, root=code:priv_dir(n2o), name, type, room=upload, data= <<>>, preview=false, size=[{200,200}], index=0, block_size=1048576}).
--record(upload, {?ELEMENT_BASE(element_upload), name, value, state=#upload_state{}, root, delegate_query, delegate_api, post_write, img_tool, post_target, size, preview}).
--record(textboxlist, {?ELEMENT_BASE(element_textboxlist), placeholder="", postback, unique=true, values=[], autocomplete=true, queryRemote=true, onlyFromValues=true, minLenght=1}).
--record(htmlbox, {?ELEMENT_BASE(element_htmlbox), html, state=#upload_state{}, root, delegate_query, delegate_api, post_write, img_tool, post_target, size}).
+-record(rtable, {?ELEMENT_BASE(element_rtable), rows=[]}).
+-record(textboxlist, {?ELEMENT_BASE(element_textboxlist), placeholder="", unique=true, values=[], autocomplete=true, queryRemote=true, onlyFromValues=true, minLenght=1}).
+-record(htmlbox, {?ELEMENT_BASE(element_htmlbox), html, state={}, root, delegate_query, delegate_api, post_write, img_tool, post_target, size}).
 
 -endif.
